e922f371f953160332d95dfdfaa556365f699747
vinaychauhan01 <147112817+vinaychauhan01@users.noreply.github.com>
2024-01-26 21:34:06 +0530

    Update info.py

--- info.py
+++ info.py
@@ -106,8 +106,8 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://raftar-bot.onrender.com/".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://raftar-bot.onrender.com/".format(FQDN, PORT)
+URL = "https://stream1-db772ce6f898.herokuapp.com/".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://stream1-db772ce6f898.herokuapp.com/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
@@ -122,9 +122,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://raftar-bot.onrender.com/".format(FQDN)
+    URL = "https://stream1-db772ce6f898.herokuapp.com/".format(FQDN)
 else:
-    URL = "https://raftar-bot.onrender.com/".format(FQDN)
+    URL = "https://stream1-db772ce6f898.herokuapp.com/".format(FQDN)
 
 
 LOG_STR = "Current Cusomized Configurations are:-\n"
